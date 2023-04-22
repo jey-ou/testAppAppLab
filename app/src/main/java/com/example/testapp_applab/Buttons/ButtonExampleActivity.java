48eec532ca42e5d5bab2ae4e20cd798881cd8b38
jey-ou <83501213+jey-ou@users.noreply.github.com>
2023-04-22 21:57:57 +0200

    aanpassingen kleuren

---
+++ app/src/main/java/com/example/testapp_applab/Buttons/ButtonExampleActivity.java
@@ -0,0 +1,16 @@
+package com.example.testapp_applab.Buttons;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.testapp_applab.R;
+
+public class ButtonExampleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_button_example);
+    }
+}
