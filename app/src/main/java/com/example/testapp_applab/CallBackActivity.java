ade9ae261172361078669cb52697d855b2e56fae
jey-ou <83501213+jey-ou@users.noreply.github.com>
2023-05-11 07:23:41 +0200

    login up register

--- app/src/main/java/com/example/testapp_applab/CallBackActivity.java
+++ app/src/main/java/com/example/testapp_applab/CallBackActivity.java
@@ -3,6 +3,7 @@ package com.example.testapp_applab;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class CallBackActivity extends AppCompatActivity {
 
@@ -10,5 +11,49 @@ public class CallBackActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_call_back);
+
+        Log.i("status","onCreate gestart");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Log.i("status","onStart() gestart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Log.i("status","onResume() gestart");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        Log.i("status","onPause() gestart");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        Log.i("status","onStop() gestart");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+
+        Log.i("status","onRestart() gestart");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        Log.i("status","onDestroy() gestart");
     }
 }
